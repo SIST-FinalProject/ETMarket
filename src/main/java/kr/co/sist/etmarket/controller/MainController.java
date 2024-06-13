5f7d69e26de2c87d651842eeeea75dbd7cf2f32e
Smoke-Bird <immio99@naver.comgit config --global user.email immio99@naver.com>
2024-06-13 14:51:47 +0900

    환경설정

--- src/main/java/kr/co/sist/etmarket/controller/MainController.java
+++ src/main/java/kr/co/sist/etmarket/controller/MainController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class MainController {
     @GetMapping("/")
     public String start(){
-        return "test/test";
+        return "main/main";
     }
 }
