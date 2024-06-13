03d89bd94618adf0a9b523a83a763efb67752c44
Smoke-Bird <immio99@naver.comgit config --global user.email immio99@naver.com>
2024-06-13 10:10:58 +0900

    환경설정

---
+++ src/main/java/kr/co/sist/etmarket/EtmarketApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sist.etmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtmarketApplication.class, args);
+	}
+
+}
