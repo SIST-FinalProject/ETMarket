788e5c270f1d68eddf3dbebd34dcb289f4b033f5
handongheee <dhhan345@gmail.com>
2024-06-17 17:35:33 +0900

    0617 아이디저장 작업중

--- src/main/java/kr/co/sist/etmarket/controller/LoginController.java
+++ src/main/java/kr/co/sist/etmarket/controller/LoginController.java
@@ -23,39 +23,52 @@ public class LoginController {
 	
 	// 로그인폼 이동
 	@GetMapping("login")
-    public String goLogin() {
+    public String goLogin(HttpSession session, Model model) {
+		
+		// 아이디 비밀번호가 일치하지 않을 경우, 세션에서 오류 메시지를 가져와 모델에 추가하고 세션에서 삭제
+        String loginError = (String) session.getAttribute("loginError");
+        if (loginError != null) {
+            model.addAttribute("loginError", loginError);
+            session.removeAttribute("loginError");
+        }
+        
     	return "login/loginForm";
     }
 	
+	
 	// 로그인 처리
 	@PostMapping("loginprocess")
-	public String loginprocess(@ModelAttribute UserDto userDto, Model model, HttpSession session) {
+	public String loginprocess(@ModelAttribute UserDto userDto, Model model, HttpSession session,
+			@RequestParam(required = false) String cbsave) {
 		
 		Optional<UserDto> loginUser=loginService.login(userDto);
 		
 		if (loginUser.isPresent()) {
 			 // 로그인 성공
-			UserDto loggedInUser = loginUser.get(); // UserDto에 로그인된 계정 정보 추가
+			UserDto loggedIn = loginUser.get(); // UserDto에(loggedIn) 로그인된 계정 정보 추가
             model.addAttribute("user", loginUser.get()); // userDto 객체를 모델에 추가
             
             session.setMaxInactiveInterval(60*60*8);
-            session.setAttribute("myid", loggedInUser.getUserLoginId());
+            session.setAttribute("myid", loggedIn.getUserLoginId());
 			session.setAttribute("loginok", "yes");
 			//session.setAttribute("saveok", cbsave);
 			
 			// 로그인 정보 확인용
-//			System.out.println("아이디 출력: "+loggedInUser.getUserLoginId());
-//			System.out.println("이름 출력: "+loggedInUser.getUserName());
-//			System.out.println("회원번호 출력: "+loggedInUser.getUserId());
+//			System.out.println("아이디 출력: "+loggedIn.getUserLoginId());
+//			System.out.println("이름 출력: "+loggedIn.getUserName());
+//			System.out.println("회원번호 출력: "+loggedIn.getUserId());
+			System.out.println("아이디 저장 확인: "+cbsave);
             
             return "redirect:/"; 
         } else {
         	 // 로그인 실패 
-            model.addAttribute("loginError", "아이디 및 비밀번호가 일치하지 않습니다."); // 에러 메시지를 모델에 추가
+            //model.addAttribute("loginError", "아이디 및 비밀번호가 일치하지 않습니다."); // 에러 메시지를 모델에 추가
+            session.setAttribute("loginError", "아이디 및 비밀번호가 일치하지 않습니다."); // 에러 메시지를 세션에 추가
            
-            return "login/loginForm"; 
+            return "redirect:login"; 
         }
 	}
+	
 	
 	// 로그아웃
 	@GetMapping("logout")
