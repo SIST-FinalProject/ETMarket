a36ae2cf82c120d06ebbff9a86a1c3b6ee719bf1
wcwcwc123 <ggul147258@gmail.com>
2024-07-01 12:12:16 +0900

    판매자 신고 완료

--- src/main/java/kr/co/sist/etmarket/controller/ReportController.java
+++ src/main/java/kr/co/sist/etmarket/controller/ReportController.java
@@ -1,12 +1,14 @@
 package kr.co.sist.etmarket.controller;
 
 
+import jakarta.servlet.http.HttpSession;
 import kr.co.sist.etmarket.dto.BasicSellerDto;
 import kr.co.sist.etmarket.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -19,11 +21,35 @@ public class ReportController {
 
 
     @GetMapping("/report/seller")
-    public String reportUser(@RequestParam("uid") Long uid, Model model) {
+    public String getReportUserForm(@RequestParam("uid") Long uid, Model model, HttpSession httpSession) {
 
         BasicSellerDto basicSellerDto = reportService.getUserIdName(uid);
+        Long myUid = (Long) httpSession.getAttribute("myUserId");
+
+        if (myUid == null) {
+            return "report/error_requireLogin";
+        }
+
+
 
         model.addAttribute("sellerDto", basicSellerDto);
         return "report/report_user";
     }
+
+    @PostMapping("/submitReportUser")
+    public String submitReportUser(@RequestParam("sellerId")Long sellerId,
+                                   @RequestParam("reportContent")String reportContent,
+                                   HttpSession httpSession) {
+
+        Long myUid = (Long) httpSession.getAttribute("myUserId");
+
+        if (myUid == null) {
+            return "redirect:/login";
+        }
+
+        reportService.submitReportUser(myUid, sellerId, reportContent);
+        return "redirect:/";
+
+    }
 }
+
--- src/main/java/kr/co/sist/etmarket/service/ReportService.java
+++ src/main/java/kr/co/sist/etmarket/service/ReportService.java
@@ -2,8 +2,10 @@ package kr.co.sist.etmarket.service;
 
 
 import kr.co.sist.etmarket.dao.ItemDetailRepository;
+import kr.co.sist.etmarket.dao.ReportUserRepository;
 import kr.co.sist.etmarket.dao.SellerDetailRepository;
 import kr.co.sist.etmarket.dto.BasicSellerDto;
+import kr.co.sist.etmarket.entity.ReportUser;
 import kr.co.sist.etmarket.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ public class ReportService {
 
     private final SellerDetailRepository sellerDetailRepository;
     private final ItemDetailRepository itemDetailRepository;
+    private final ReportUserRepository reportUserRepository;
 
     public BasicSellerDto getUserIdName(Long userId) {
 
@@ -27,10 +30,19 @@ public class ReportService {
     }
 
 
+    public void submitReportUser(Long userId, Long sellerId, String reportContent) {
+
+        ReportUser reportUser = new ReportUser();
+
+        User reporter = sellerDetailRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("id값에 해당하는 유저가 없습니다"));
+        User reported = sellerDetailRepository.findById(sellerId).orElseThrow(() -> new NoSuchElementException("id값에 해당하는 유저가 없습니다"));
 
 
 
+        reportUser.setReporter(reporter);
+        reportUser.setReported(reported);
+        reportUser.setContent(reportContent);
 
-
-
+        reportUserRepository.save(reportUser);
+    }
 }
